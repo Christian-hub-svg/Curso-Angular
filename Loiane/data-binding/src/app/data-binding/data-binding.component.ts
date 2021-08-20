74e3d1ea615e13da0ef208b27f82652f598e2405
Christian-hub-svg <christian.hamai@bandtec.com.br>
2021-08-20 13:27:25 -0300

    parte de binding finalizada

--- Loiane/data-binding/src/app/data-binding/data-binding.component.ts
+++ Loiane/data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,18 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight {
+          background-color: yellow;
+          font-weight: bold;
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   nome: string = 'Christian';
   cursoAngular: boolean = true;
-  urlImagem: string = 'http://lorempixel.com.br/400/200';
+  urlImagem = 'http://lorempixel.com.br/400/200';
 
-  constructor() { }
+  valorAtual: string = '';
+  valorSalvo = '';
 
-  ngOnInit(): void {
-  }
+  isMouseOver: boolean = false;
+
+  nomeDoCurso: string = 'Angular';
+
+  valorInicial = 15;
 
   getValor(){
     return 1;
@@ -22,6 +34,31 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento: any){
+    console.log(evento.novoValor);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
   }
 
 }
